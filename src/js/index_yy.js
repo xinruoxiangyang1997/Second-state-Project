c03818b9670d0188cec08d089a5bcf6e0e9043d2
杨健平 <471711253@qq.com>
2018-10-10 11:44:23 +0800

    2018/10/10/12:00

--- src/js/index_yy.js
+++ src/js/index_yy.js
@@ -1,8 +1,4 @@
 $(function() {
-    $(".indexbase-top").load("http://10.31.157.28:8080/Project_YY/Second-state-Project/src/html/index_top.html");
-    $(".indexbase-btm").load("http://10.31.157.28:8080/Project_YY/Second-state-Project/src/html/index_btm.html");
-    $(".navbar-toggle").on('click', function() {
-        $("this").hide();
-
-    });
+    // $(".indexbase-btm").load("http://10.31.157.28:8080/Project_YY/Second-state-Project/src/html/index_btm.html");
+    // $(".indexbase-fix").load("http://10.31.157.28:8080/Project_YY/Second-state-Project/src/html/fix-right.html");
 })
